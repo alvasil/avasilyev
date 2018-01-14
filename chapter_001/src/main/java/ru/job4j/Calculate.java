a6574e25ea875654f2d2600313c3ac4109309fec
alvasil <aln.vasilyev@gmail.com>
2018-01-14 15:50:54 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**
+* Task 1.3. JavaDoc.
+* @author Vasilyev Alexander.
+*/
+
 public class Calculate {
+/**
+* Method displays a text.
+*/
 	public static void main (String[] args) {
-	System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Calculate task.
+*
+* @author Alexander Vasilyev (aln.vasilyev@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
