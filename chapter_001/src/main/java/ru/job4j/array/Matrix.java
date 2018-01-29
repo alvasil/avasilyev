f804d2680f35ba2a950328bd3287643c5429a167
alvasil <aln.vasilyev@gmail.com>
2018-01-29 21:43:06 +0300

    5.4. Двухмерный массив. Таблица умножения. ver 2

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -1,13 +1,13 @@
 package ru.job4j.array;
 
 public class Matrix {
-    int[][] multiple(int size) {
-        int[][] data = new int[size][size];
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                data[i][j] = i * j;
-            }
-        }
-        return data;
-    }
+	int[][] multiple(int size) {
+		int[][] data = new int[size][size];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data.length; j++) {
+				data[i][j] = (i + 1) * (j + 1);
+			}
+		}
+		return data;
+	}
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -6,10 +6,17 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MatrixTest {
-    @Test
-    public void whenTwoMultiplyTwoThenFour() {
-        Matrix matrix = new Matrix();
-        int[][] result = matrix.multiple(5);
-        assertThat(result[2][2], is (4));
-    }
+	@Test
+	public void whenTwoMultiplyTwoThenFour() {
+		Matrix matrix = new Matrix();
+		int[][] result = matrix.multiple(5);
+		assertThat(result[1][1], is(4));
+	}
+
+	@Test
+	public void whenOneMultiplyOneThenOne() {
+		Matrix matrix = new Matrix();
+		int[][] result = matrix.multiple(5);
+		assertThat(result[0][0], is(1));
+	}
 }
