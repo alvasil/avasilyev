b6dec41f2cb848fc24b3dff10ad573fd51cb2b21
alvasil <aln.vasilyev@gmail.com>
2018-02-02 20:25:08 +0300

    Тестовое задание - реализовать копирование из двух массивов в один массив ver 2

--- chapter_001/src/test/java/ru/job4j/array/TripleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TripleSortTest.java
@@ -6,10 +6,18 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class TripleSortTest {
 	@Test
-	public void whenTwoUnsortedArraysThenOneSorted() {
+	public void whenTwoUnsortedArraysAndFirstBiggerThenOneSorted() {
 		TripleSort twoArrays = new TripleSort();
-		int[] result = twoArrays.bigSort(new int[] {3, 4, 1, 7}, new int[] {5, 2, 6});
+		int[] result = twoArrays.bigSort(new int[] {1, 3, 5, 7}, new int[] {2, 4, 6});
 		int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7};
+		assertArrayEquals(result, expected);
+	}
+
+	@Test
+	public void whenTwoUnsortedArraysAndSecondBiggerThenOneSorted() {
+		TripleSort twoArrays = new TripleSort();
+		int[] result = twoArrays.bigSort(new int[] {1, 3}, new int[] {2, 4, 5, 6});
+		int[] expected = new int[] {1, 2, 3, 4, 5, 6};
 		assertArrayEquals(result, expected);
 	}
 }
