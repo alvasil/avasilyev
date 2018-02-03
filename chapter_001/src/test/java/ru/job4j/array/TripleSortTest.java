736c446fae8385befb19d665630afcb86a420eca
alvasil <aln.vasilyev@gmail.com>
2018-02-03 12:53:49 +0300

    Тестовое задание _тесты  ver 3

--- chapter_001/src/test/java/ru/job4j/array/TripleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TripleSortTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertArrayEquals;
 
 
@@ -8,24 +9,32 @@ public class TripleSortTest {
 	@Test
 	public void whenTwoSortedArraysAndFirstBiggerThenOneSorted() {
 		TripleSort twoArrays = new TripleSort();
-		int[] result = twoArrays.bigSort(new int[] {1, 3, 5, 7}, new int[] {2, 4, 6});
-		int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7};
+		int[] result = twoArrays.bigSort(new int[]{1, 3, 5, 7}, new int[]{2, 4, 6});
+		int[] expected = new int[]{1, 2, 3, 4, 5, 6, 7};
 		assertArrayEquals(result, expected);
 	}
 
 	@Test
 	public void whenTwoSortedArraysAndSecondBiggerThenOneSorted() {
 		TripleSort twoArrays = new TripleSort();
-		int[] result = twoArrays.bigSort(new int[] {1, 3}, new int[] {2, 4, 5, 6});
-		int[] expected = new int[] {1, 2, 3, 4, 5, 6};
+		int[] result = twoArrays.bigSort(new int[]{1, 3}, new int[]{2, 4, 5, 6});
+		int[] expected = new int[]{1, 2, 3, 4, 5, 6};
 		assertArrayEquals(result, expected);
 	}
 
 	@Test
 	public void whenTwoSortedArraysAndFirstHaveEqualsThenOneSorted() {
 		TripleSort twoArrays = new TripleSort();
-		int[] result = twoArrays.bigSort(new int[] {1, 1, 3}, new int[] {4, 5, 6});
-		int[] expected = new int[] {1, 1, 3, 4, 5, 6};
+		int[] result = twoArrays.bigSort(new int[]{1, 1, 3}, new int[]{4, 5, 6});
+		int[] expected = new int[]{1, 1, 3, 4, 5, 6};
+		assertArrayEquals(result, expected);
+	}
+
+	@Test
+	public void whenTwoSortedArraysAndSecondHaveEqualsThenOneSorted() {
+		TripleSort twoArrays = new TripleSort();
+		int[] result = twoArrays.bigSort(new int[]{1, 2, 3}, new int[]{4, 4});
+		int[] expected = new int[]{1, 2, 3, 4, 4};
 		assertArrayEquals(result, expected);
 	}
 }
