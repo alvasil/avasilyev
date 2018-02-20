12bbd80774aab7d3e6510f3e2ee4d00d296bfab6
alvasil <aln.vasilyev@gmail.com>
2018-02-20 13:34:09 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,17 +11,24 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+	// поле содержит дефолтный вывод в консоль.
+	private final PrintStream stdout = System.out;
+	// буфер для результата.
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+	@Before
+	public void loadOutput() {
+		System.setOut(new PrintStream(this.out));
+	}
+
+	@After
+	public void backOutput() {
+		System.setOut(this.stdout);
+	}
+
 	@Test
 	public void whenDrawSquare() {
-		// получаем ссылку на стандартный вывод в консоль.
-		PrintStream stdout = System.out;
-		// Создаем буфер для хранения вывода.
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		//Заменяем стандартный вывод на вывод в пямять для тестирования.
-		System.setOut(new PrintStream(out));
-		// выполняем действия пишушиее в консоль.
 		new Paint(new Square()).draw();
-		// проверяем результат вычисления
 		assertThat(
 				new String(out.toByteArray()),
 				is(
@@ -32,14 +41,10 @@ public class PaintTest {
 								.toString()
 				)
 		);
-		// возвращаем обратно стандартный вывод в консоль.
-		System.setOut(stdout);
 	}
+
 	@Test
 	public void whenDrawTriangle() {
-		PrintStream stdout = System.out;
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
 		new Paint(new Triangle()).draw();
 		assertThat(
 				new String(out.toByteArray()),
@@ -52,6 +57,5 @@ public class PaintTest {
 								.toString()
 				)
 		);
-		System.setOut(stdout);
 	}
 }
