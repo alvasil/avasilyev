ec72bd3c92c8f2d21a12c4afa9a16e6d231659c5
alvasil <aln.vasilyev@gmail.com>
2018-03-08 15:57:39 +0300

    2. Очередь с приоритетом на LinkedList [#41670]. ver2

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,15 +5,17 @@ import java.util.LinkedList;
 public class PriorityQueue {
 	private LinkedList<Task> tasks = new LinkedList<>();
 
-
 	public void put(Task task) {
 		if (this.tasks.size() == 0) {
-			this.tasks.add(task);
-		} else if (this.tasks.size() >= 1) {
-			if (task != null && task.getPriority() < this.tasks.getFirst().getPriority()) {
-				this.tasks.addFirst(task);
-			} else {
-				this.tasks.addLast(task);
+			this.tasks.addFirst(task);
+		} else if (task != null && task.getPriority() < this.tasks.getFirst().getPriority()) {
+			this.tasks.addFirst(task);
+		} else {
+			for (int i = 0; i < this.tasks.size(); i++) {
+				if (task != null && task.getPriority() > this.tasks.get(i).getPriority() && task.getPriority() < this.tasks.get(i + 1).getPriority()) {
+					this.tasks.add(i + 1, task);
+					break;
+				}
 			}
 		}
 	}
--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -8,9 +9,11 @@ public class PriorityQueueTest {
 	@Test
 	public void whenHigherPriority() {
 		PriorityQueue queue = new PriorityQueue();
-		queue.put(new Task("last", 5));
-		queue.put(new Task("first", 1));
-		queue.put(new Task("middle", 3));
+		queue.put(new Task("last", 10));
+		queue.put(new Task("first", 2));
+		queue.put(new Task("third", 5));
+		queue.put(new Task("fourth", 8));
+		queue.put(new Task("second", 3));
 		Task result = queue.take();
 		assertThat(result.getDesc(), is("first"));
 	}
