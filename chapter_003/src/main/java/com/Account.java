328ce064821f7dd4e9543adc45fcc8166ec1d8bb
alvasil <aln.vasilyev@gmail.com>
2018-03-16 22:32:25 +0300

    ~~peer review commented

--- chapter_003/src/main/java/com/Account.java
+++ chapter_003/src/main/java/com/Account.java
@@ -1,57 +1,57 @@
-package com;
-
-
-public class Account {
-
-	double values; //поля желательно сделать приватными, чтобы соблюсти принцип инкапсуляции
-	String reqs;
-
-	public Account(double values, String requisites) {
-		this.values = values;
-		this.reqs = requisites;
-	}
-
-	public double getValues() {
-		return this.values;
-	}
-
-	public String getReqs() {
-		return this.reqs;
-	}
-
-	boolean transfer(Account destination, double amount) {
-		boolean success = false;
-		if (amount > 0 && amount < this.values && destination != null) { // надо указать amount <= this.values, чтобы можно было перевести всю сумму со счета
-			success = true;
-			this.values -= amount;
-			destination.values += amount;
-		}
-		return success;
-	}
-
-	// указать @Override
-	public String toString() {
-		String otvet; //можно объединить объявление переменной с ее инициализацией
-		otvet = "Account{" + "values=" + values + ", reqs='" + reqs + "\\" + "}";
-		return otvet;
-	}
-	// указать @Override
-	// equals и hashCode должен еще учитывать поля для понятия эквивалентности
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-
-		Account account = (Account) o;
-
-		return this.reqs.equals(account.reqs);
-	}
-
-	// указать @Override
-	public int hashCode() {
-		return this.reqs.hashCode();
-	}
-}
+//package com;
+//
+//
+//public class Account {
+//
+//	double values; //поля желательно сделать приватными, чтобы соблюсти принцип инкапсуляции
+//	String reqs;
+//
+//	public Account(double values, String requisites) {
+//		this.values = values;
+//		this.reqs = requisites;
+//	}
+//
+//	public double getValues() {
+//		return this.values;
+//	}
+//
+//	public String getReqs() {
+//		return this.reqs;
+//	}
+//
+//	boolean transfer(Account destination, double amount) {
+//		boolean success = false;
+//		if (amount > 0 && amount < this.values && destination != null) { // надо указать amount <= this.values, чтобы можно было перевести всю сумму со счета
+//			success = true;
+//			this.values -= amount;
+//			destination.values += amount;
+//		}
+//		return success;
+//	}
+//
+//	// указать @Override
+//	public String toString() {
+//		String otvet; //можно объединить объявление переменной с ее инициализацией
+//		otvet = "Account{" + "values=" + values + ", reqs='" + reqs + "\\" + "}";
+//		return otvet;
+//	}
+//	// указать @Override
+//	// equals и hashCode должен еще учитывать поля для понятия эквивалентности
+//	public boolean equals(Object o) {
+//		if (this == o) {
+//			return true;
+//		}
+//		if (o == null || getClass() != o.getClass()) {
+//			return false;
+//		}
+//
+//		Account account = (Account) o;
+//
+//		return this.reqs.equals(account.reqs);
+//	}
+//
+//	// указать @Override
+//	public int hashCode() {
+//		return this.reqs.hashCode();
+//	}
+//}
--- chapter_003/src/main/java/com/Convert.java
+++ chapter_003/src/main/java/com/Convert.java
@@ -1,43 +1,43 @@
-package com;
-
-
-import java.util.*;
-
-public class Convert {
-
-	//объявлен конструктор по умолчанию - это требуется в том случае, если мы еще объявили парамет.конструктор и нам
-	// необходим еще и конструктор по умолчанию.
-	// в данном случае конструктор по умолчанию можно не указывать явно.
-	public Convert() {
-
-	}
-
-	//Converts array to list
-	// не понятно для чего указан дефолтный модификатор доступа
-	List<Integer> makeList(int[][] array) {
-		ArrayList<Integer> list = new ArrayList<>();
-		for (int i = 0; i < array.length; i++) { //можно заменить на foreach
-			for (int j = 0; j < array[i].length; j++)
-				list.add(array[i][j]);
-		}
-		return list;
-	}
-
-	//Converts list to array
-	public int[][] makeArray(List<Integer> list, int rws) {
-		Iterator<Integer> iterator = list.iterator();
-		int cls = list.size() / rws + (list.size() % rws == 0 ? 0 : 1);
-
-		int[][] array = new int[rws][cls];
-		for (int i = 0; i < rws; i++) {
-			for (int j = 0; j < cls; j++) {
-				// не хватает фигурных скобок у if else
-				if (iterator.hasNext())
-					array[i][j] = iterator.next();
-				else
-					array[i][j] = 0;
-			}
-		}
-		return array;
-	}
-}
+//package com;
+//
+//
+//import java.util.*;
+//
+//public class Convert {
+//
+//	//объявлен конструктор по умолчанию - это требуется в том случае, если мы еще объявили парамет.конструктор и нам
+//	// необходим еще и конструктор по умолчанию.
+//	// в данном случае конструктор по умолчанию можно не указывать явно.
+//	public Convert() {
+//
+//	}
+//
+//	//Converts array to list
+//	// не понятно для чего указан дефолтный модификатор доступа
+//	List<Integer> makeList(int[][] array) {
+//		ArrayList<Integer> list = new ArrayList<>();
+//		for (int i = 0; i < array.length; i++) { //можно заменить на foreach
+//			for (int j = 0; j < array[i].length; j++)
+//				list.add(array[i][j]);
+//		}
+//		return list;
+//	}
+//
+//	//Converts list to array
+//	public int[][] makeArray(List<Integer> list, int rws) {
+//		Iterator<Integer> iterator = list.iterator();
+//		int cls = list.size() / rws + (list.size() % rws == 0 ? 0 : 1);
+//
+//		int[][] array = new int[rws][cls];
+//		for (int i = 0; i < rws; i++) {
+//			for (int j = 0; j < cls; j++) {
+//				// не хватает фигурных скобок у if else
+//				if (iterator.hasNext())
+//					array[i][j] = iterator.next();
+//				else
+//					array[i][j] = 0;
+//			}
+//		}
+//		return array;
+//	}
+//}
