30bfa8ab0f983a65a678f3e0ec0fa9d79e62fecc
alvasil <aln.vasilyev@gmail.com>
2018-03-26 14:58:36 +0300

    4.2. Set linked based [#997] ver 2.

---
+++ chapter_004/src/main/java/ru/job4j/list/AbstractLinked.java
@@ -0,0 +1,64 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public abstract class AbstractLinked<E> implements Iterable {
+	private Node<E> first;
+	private Node<E> last;
+	private int size = 0;
+	private int modCount = 0;
+
+	public Node<E> getFirst() {
+		return first;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void add(E value) {
+		Node<E> newNode = new Node<>(last, value, null);
+		if (newNode.getPrev() == null) {
+			this.first = newNode;
+		} else {
+			this.last.next = newNode;
+		}
+		this.last = newNode;
+		this.modCount++;
+		this.size++;
+	}
+
+	@Override
+	public Iterator iterator() {
+		return new Iterator() {
+			private Node<E> current;
+			int expectedModCount = modCount;
+			int nextCount = 0;
+
+			@Override
+			public boolean hasNext() {
+				checkForModification();
+				return (current != null && current.next != null);
+			}
+
+			@Override
+			public E next() {
+				checkForModification();
+				if (nextCount == 0) {
+					current = first;
+					nextCount++;
+				} else {
+					current = current.next;
+				}
+				return current.element;
+			}
+
+			final void checkForModification() {
+				if (modCount != expectedModCount) {
+					throw new ConcurrentModificationException();
+				}
+			}
+		};
+	}
+}
--- chapter_004/src/main/java/ru/job4j/list/Node.java
+++ chapter_004/src/main/java/ru/job4j/list/Node.java
@@ -1,26 +1,39 @@
 package ru.job4j.list;
 
-public class Node<T> {
-	T value;
-	Node<T> next;
+public class Node<E> {
+	public E element;
+	public Node<E> prev;
+	public Node<E> next;
 
-	public Node(T value) {
-		this.value = value;
+	public Node(Node<E> prev, E element, Node<E> next) {
+		this.element = element;
+		this.prev = prev;
+		this.next = next;
 	}
 
+	public E getElement() {
+		return element;
+	}
 
-	boolean hasCycle(Node first) {
-		boolean result = false;
-		Node test = first;
-		while (test.next != first) {
-			test = test.next;
-			result = true;
-			if (test.next == null) {
-				result = false;
-				break;
-			}
-		}
-		return result;
+	public void setElement(E element) {
+		this.element = element;
+	}
+
+	public Node<E> getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node<E> prev) {
+		this.prev = prev;
+	}
+
+	public Node<E> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<E> next) {
+		this.next = next;
 	}
 }
+
 
