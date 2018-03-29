0fe2bef25376ad27dbde8315b03a7cecd56811ea
alvasil <aln.vasilyev@gmail.com>
2018-03-29 16:12:52 +0300

    5.5. User hash, equals

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,43 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class User {
+	private String name;
+	private int children;
+	private Calendar birthday;
+
+	public User(String name, int children) {
+		this.name = name;
+		this.children = children;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return children == user.children &&
+				Objects.equals(name, user.name) &&
+				Objects.equals(birthday, user.birthday);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, children, birthday);
+	}
+
+	public static void main(String[] args) {
+		User first = new User("alex", 2);
+		User second = new User("alex", 2);
+		Map<User, Object> map = new HashMap<>();
+		map.put(first, "first");
+		map.put(second, "second");
+		System.out.println(map);
+	}
+}
+
+
