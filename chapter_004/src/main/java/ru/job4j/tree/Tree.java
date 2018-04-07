4381481fda788eb24f83d1ad503ed40514d45430
alvasil <aln.vasilyev@gmail.com>
2018-04-07 17:57:32 +0300

    Simple Tree ver.2

--- chapter_004/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_004/src/main/java/ru/job4j/tree/Tree.java
@@ -4,9 +4,39 @@ import java.util.*;
 
 public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
 	private Node<E> root;
+	private int count;
 
 	public Tree(E root) {
 		this.root = new Node<>(root);
+	}
+
+	/**
+	 * Метод должен проверять количество дочерних элементов в дереве.
+	 * Если их <= 2 - то дерево бинарное.
+	 *
+	 * @return - true, если дочек <= 2
+	 */
+	public boolean isBinary() {
+		boolean result = false;
+		if (this.root.getChildren().size() <= 2) {
+			result = true;
+			if (this.root.getChildren().size() != 0) {
+				Node<E> current = this.root;
+				while (current.getChildren().size() > 0) {
+					for (Node e : current.getChildren()) {
+						if (e.getChildren().size() <= 2) {
+							result = true;
+							current = e;
+							break;
+						} else {
+							result = false;
+						}
+					}
+				}
+			}
+
+		}
+		return result;
 	}
 
 	@Override
@@ -15,28 +45,24 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
 		if (this.root.eqValue(parent)) {
 			if (!this.root.haveDoubles(child)) {
 				this.root.add(new Node<>(child));
+				count++;
 				result = true;
 			} else {
 				result = false;
 			}
 		} else {
-			Node<E> current = this.root;
-			while (!current.eqValue(parent)) {
-				for (Node ch : current.getChildren()) {
-					if (ch.eqValue(parent)) {
-						ch.add(new Node(child));
-						current.value = parent;
-						result = true;
-						break;
-					} else {
-						current = ch;
-					}
+			if (findBy(child).isPresent()) {
+				result = false;
+			} else {
+				if (findBy(parent).isPresent()) {
+					Node<E> current = findBy(parent).orElse(new Node<>(parent));
+					current.add(new Node<>(child));
+					count++;
 				}
 			}
 		}
 		return result;
 	}
-
 
 	@Override
 	public Optional<Node<E>> findBy(E value) {
@@ -59,14 +85,35 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
 	@Override
 	public Iterator iterator() {
 		return new Iterator() {
+			Queue<Node<E>> queue = new ArrayDeque<>();
+			Node<E> current = root;
+			int itCount;
+
+			/**
+			 * анонимный инициализатор
+			 */ {
+				if (current == root) {
+					queue.offer(root);
+				}
+				for (Node<E> ch : current.getChildren()) {
+					queue.offer(ch);
+					current = ch;
+				}
+				for (Node<E> ch : current.getChildren()) {
+					queue.offer(ch);
+				}
+			}
+
+
 			@Override
 			public boolean hasNext() {
-				return false;
+				return count >= itCount;
 			}
 
 			@Override
 			public Object next() {
-				return null;
+				itCount++;
+				return queue.poll().value;
 			}
 		};
 	}
--- chapter_004/src/test/java/ru/job4j/tree/TreeTest.java
+++ chapter_004/src/test/java/ru/job4j/tree/TreeTest.java
@@ -2,6 +2,8 @@ package ru.job4j.tree;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -15,11 +17,13 @@ public class TreeTest {
 	public void when6ElFindLastThen6() {
 		Tree<Integer> tree = new Tree<>(1);
 		tree.add(1, 2);
-		boolean test = tree.add(1, 2);
-		assertFalse(test);
+		boolean testParent = tree.add(1, 2);
+		assertFalse(testParent);
 		tree.add(1, 3);
 		tree.add(1, 4);
 		tree.add(4, 5);
+		boolean testChild = tree.add(4, 5);
+		assertFalse(testChild);
 		tree.add(5, 6);
 		assertThat(tree.findBy(6).isPresent(), is(true));
 	}
@@ -29,5 +33,49 @@ public class TreeTest {
 		Tree<Integer> tree = new Tree<>(1);
 		tree.add(1, 2);
 		assertThat(tree.findBy(7).isPresent(), is(false));
+	}
+
+	@Test
+	public void whenTreeIsBinary() {
+		Tree<Integer> tree = new Tree<>(0);
+		tree.add(0, 1);
+		tree.add(0, 2);
+		tree.add(1, 3);
+		tree.add(1, 4);
+		boolean result = tree.isBinary();
+		assertTrue(result);
+	}
+
+	@Test
+	public void whenTreeIsNotBinary() {
+		Tree<Integer> tree = new Tree<>(0);
+		tree.add(0, 1);
+		tree.add(0, 2);
+		tree.add(0, 3);
+		tree.add(1, 3);
+		tree.add(1, 4);
+		boolean result = tree.isBinary();
+		assertFalse(result);
+	}
+
+	@Test
+	public void whenIterate() {
+		Tree<Integer> tree = new Tree<>(1);
+		tree.add(1, 2);
+		tree.add(1, 3);
+		tree.add(1, 4);
+		tree.add(4, 5);
+
+		Iterator it = tree.iterator();
+		assertThat(it.next(), is(1));
+		assertTrue(it.hasNext());
+		assertThat(it.next(), is(2));
+		assertTrue(it.hasNext());
+		assertThat(it.next(), is(3));
+		assertTrue(it.hasNext());
+		assertThat(it.next(), is(4));
+		assertTrue(it.hasNext());
+		assertThat(it.next(), is(5));
+		assertFalse(it.hasNext());
 	}
 }
