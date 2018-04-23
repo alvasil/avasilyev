0a7660ac8d7bc9005d119a8eb52a1ea5829d2673
alvasil <aln.vasilyev@gmail.com>
2018-04-23 15:46:01 +0300

    Car catalogue [#1733]

---
+++ chapter_006/src/main/java/sql/car/car_catalogue_requests.sql
@@ -0,0 +1,33 @@
+﻿create table transmission (
+	id serial primary  key,
+	name varchar(100)
+);
+
+create table engine (
+	id serial primary  key,
+	name varchar(100)
+);
+
+create table gearbox (
+	id serial primary  key,
+	name varchar(100)
+);
+
+create table car (
+	id serial primary  key,
+	name varchar(100),
+	transmission_id int references transmission(id),
+	engine_id int references engine(id),
+	gearbox_id int references gearbox(id)
+);
+
+select * from car;
+
+--unused transmission
+select t.name from transmission as t left outer join car as c on c.id = t.id where c.id is null;
+
+--unused engine
+select e.name from engine as e left outer join car as c on c.id = e.id where c.id is null;
+
+--unused gearbox
+select g.name from car as c right outer join gearbox as g on c.id = g.id where c.id is null;
---
+++ chapter_006/src/main/java/sql/car/car_catalogue_values.sql
@@ -0,0 +1,21 @@
+﻿insert into transmission (name) values ('trans 1');
+insert into transmission (name) values ('trans 2');
+insert into transmission (name) values ('trans 3');
+insert into transmission (name) values ('trans 4');
+insert into transmission (name) values ('trans 5');
+
+insert into engine (name) values ('engine 1');
+insert into engine (name) values ('engine 2');
+insert into engine (name) values ('engine 3');
+insert into engine (name) values ('engine 4');
+insert into engine (name) values ('engine 5');
+
+insert into gearbox (name) values ('gearbox 1');
+insert into gearbox (name) values ('gearbox 2');
+insert into gearbox (name) values ('gearbox 3');
+insert into gearbox (name) values ('gearbox 4');
+insert into gearbox (name) values ('gearbox 5');
+
+insert into car (name, transmission_id, engine_id, gearbox_id) values ('car 1', 1, 1, 1);
+insert into car (name, transmission_id, engine_id, gearbox_id) values ('car 2', 2, 2, 2);
+insert into car (name, transmission_id, engine_id, gearbox_id) values ('car 3', 3, 3, 3);
