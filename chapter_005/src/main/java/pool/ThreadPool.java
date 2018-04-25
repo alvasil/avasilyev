2e43dd7466f74964c3e314b7299ed4ad209c373d
alvasil <aln.vasilyev@gmail.com>
2018-04-25 11:39:38 +0300

    2. ThreadPool [#1099] ver.2

--- chapter_005/src/main/java/pool/ThreadPool.java
+++ chapter_005/src/main/java/pool/ThreadPool.java
@@ -13,37 +13,39 @@ public class ThreadPool {
 	@GuardedBy("this")
 	private final Queue<Work> pool = new LinkedList<>();
 
+	private WorkThread[] threads;
+
+	public ThreadPool() {
+		this.threads = new WorkThread[nProcessors];
+		for (int i = 0; i < nProcessors; i++) {
+			threads[i] = new WorkThread();
+			threads[i].start();
+		}
+	}
+
 	public synchronized void add(Work work) {
 		this.pool.add(work);
 		notifyAll();
 	}
 
-	/**
-	 * Создает потоки в кол-ве равном кол-ву процессоров
-	 * Если пул пустой - ожидание
-	 */
-	public void execute() {
-		for (int i = 0; i < nProcessors; i++) {
-			new Thread() {
-				@Override
-				public void run() {
-					Work work;
+	public class WorkThread extends Thread {
+		@Override
+		public void run() {
+			Work work;
 
-					while (true) {
-						synchronized (pool) {
-							while (pool.isEmpty()) {
-								try {
-									pool.wait();
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-							}
-							work = pool.poll();
+			while (true) {
+				synchronized (pool) {
+					while (pool.isEmpty()) {
+						try {
+							pool.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
 						}
-						work.doWork();
 					}
+					work = pool.poll();
 				}
-			};
+				work.doWork();
+			}
 		}
 	}
 }
