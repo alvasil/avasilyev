80849dfddc9b5e027540c0589e928acb4afe00ca
alvasil <aln.vasilyev@gmail.com>
2018-04-26 19:15:41 +0300

    person - company, add.task [#2760]

---
+++ chapter_006/src/main/java/sql/person_company_task.sql
@@ -0,0 +1,7 @@
+﻿--1
+select p.name from person as p where id not in (5);
+
+select p.name, c.name from person as p inner join company as c on c.id = p.company_id;
+
+--2
+select company.name, count(person.id) from company, person where company.id = person.company_id group by company.name limit 1;
